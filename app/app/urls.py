d69562bf97444032ef125f5df01685d645aa1e6e
MaciejRud <m.rudol93@gmail.com>
2024-09-09 11:12:38 +0200

    Check new drf version and fix problems.

--- app/app/urls.py
+++ app/app/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name= 'api-schema'),
     path('api/docs/',
-         SpectacularSwaggerView.as_view('api-schema'),
+         SpectacularSwaggerView.as_view(url_name='api-schema'),
          name = ('api-docs')
     )
 ]
