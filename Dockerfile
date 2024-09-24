7e599388fabfd6f38b6bd5430231c14c9df77852
MaciejRud <m.rudol93@gmail.com>
2024-09-24 17:43:08 +0200

    Addin login and authentication with token on local storage.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.12-alpine3.19
 LABEL maintainer='maciejrudol@gmail.com'
 
-ENV PYTHONUNBUFFERED 1
+ENV PYTHONUNBUFFERED=1
 
 COPY ./requirements.txt /tmp/requirements.txt
 COPY ./requirements.dev.txt /tmp/requirements.dev.txt
--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,5 +1,30 @@
 window.addEventListener('DOMContentLoaded', event => {
 
+    const loginToggle = document.getElementById('login-toggle');
+    const loginFormWrapper = document.getElementById('login-form-wrapper');
+    const loginForm = document.getElementById('login-form');
+    const loginError = document.getElementById('login-error');
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('value');
+
+    const token = localStorage.getItem('authToken');
+    if (token) {
+        // Użytkownik jest zalogowany, pokaż przycisk "Wyloguj"
+        loginToggle.innerHTML = `
+                <span class="d-flex align-items-center">
+                    <i class="bi-box-arrow-right me-2"></i>
+                    <span class="small">Wyloguj</span>
+                </span>`;
+        loginToggle.classList.remove('btn-primary');
+        loginToggle.classList.add('btn-danger');
+
+        // Obsługa wylogowania
+        loginToggle.addEventListener('click', function() {
+            // Usuń token i odśwież stronę
+            localStorage.removeItem('authToken');
+            location.reload();
+        });
+    }
+
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
@@ -22,4 +47,61 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // Przełączanie widoczności formularza logowania
+    loginToggle.addEventListener('click', function() {
+        loginFormWrapper.classList.toggle('d-none');
+    });
+
+    // Obsługa logowania
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const formData = new FormData(loginForm);
+        const data = {
+            email: formData.get('email'),
+            password: formData.get('password')
+        };
+
+        fetch(loginUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Nieprawidłowe dane logowania');
+            }
+        })
+        .then(data => {
+            // Zapisz token w localStorage
+            localStorage.setItem('authToken', data.token);
+
+            // Ukryj formularz logowania
+            loginFormWrapper.classList.add('d-none');
+
+            // Zmień przycisk "Zaloguj" na "Wyloguj"
+            loginToggle.innerHTML = `
+                <span class="d-flex align-items-center">
+                    <i class="bi-box-arrow-right me-2"></i>
+                    <span class="small">Wyloguj</span>
+                </span>`;
+            loginToggle.classList.remove('btn-primary');
+            loginToggle.classList.add('btn-danger');
+
+            // Obsługa wylogowania
+            loginToggle.addEventListener('click', function() {
+                // Usuń token i odśwież stronę
+                localStorage.removeItem('authToken');
+                location.reload();
+            });
+        })
+        .catch(error => {
+            loginError.textContent = error.message;
+            loginError.classList.remove('d-none');
+        });
+    });
 });
